9c035a8f9cc529bafcbe486092368712d68ab984
Clément Tessier <clement.tessier@capgemini.com>
2022-10-06 08:10:09 +0200

    update response

--- MSIPLookup/index.js
+++ MSIPLookup/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const ip = req.headers['x-forwarded-for'].split(':')[0];
-    const message = `L'adresse IP ci-dessous est votre adresse IP de sortie, telle que vue par les services Microsoft.<br /><br />${ip}`;
+    const message = `${ip} (seen from Microsoft infrastructures)`;
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: message
